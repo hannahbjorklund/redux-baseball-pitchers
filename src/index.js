f4f7fad6a2a5b016fa58b29e0878f543a60727a8
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-11-28 17:22:38 -0600

    Did setup for redux and created some reducers for pitcher and catcher list

--- src/index.js
+++ src/index.js
@@ -2,9 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+// Reducers
+const pitcherList = (state=[], action) => {
+    if (action.type === 'ADD_PITCHER') {
+      const newPitcher = action.payload;
+      console.log("Adding a new pitcher:", newPitcher);
+      return [...state, newPitcher];
+    }
+    return state;
+}
+
+const catcherList = (state=[], action) => {
+    if(action.type === 'ADD_CATCHER'){
+        const newCatcher = action.payload;
+        console.log("Adding a new catcher:", newCatcher);
+        return [...state, newCatcher];
+    }
+    return state;
+}
+
+// Creating our bucket
+const reduxStore = createStore(
+    combineReducers({
+      pitcherList,
+      catcherList
+    })
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <Provider store={reduxStore}>
         <App />
-    </React.StrictMode>
+    </Provider>
 );
